294ee36ecb587f2c656a1af3c041b41a32731ef7
Vona Csaba <vcsneu@gmail.com>
2021-04-26 11:39:53 +0200

    cors test

--- lets-trade/src/app/app.component.ts
+++ lets-trade/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { CorsService } from './cors.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'lets-trade';
+  title = 'asd';
+  constructor(private cors: CorsService) {}
+
+  test() {
+    this.cors.test();
+  }
 }
--- lets-trade/src/app/app.module.ts
+++ lets-trade/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CorsComponent } from './cors/cors.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, CorsComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot([{ path: 'cors', component: CorsComponent }]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
