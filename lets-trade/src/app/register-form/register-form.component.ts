fc51a03ec04b382bb12f8fd49da81daa28d310f5
Vona Csaba <vcsneu@gmail.com>
2021-04-27 13:33:03 +0200

    client validation (#4)

--- lets-trade/src/app/register-form/register-form.component.ts
+++ lets-trade/src/app/register-form/register-form.component.ts
@@ -9,17 +9,27 @@ import { FormBuilder } from '@angular/forms';
 })
 export class RegisterFormComponent implements OnInit {
   sendForm = this.formBuilder.group({ name: '', password: '', email: '' });
+  errorMessage = '';
   constructor(
     private formBuilder: FormBuilder,
     private tradeApi: TradeApiService
   ) {}
 
   onSubmit() {
-    this.tradeApi.register(
-      this.sendForm.value.name,
-      this.sendForm.value.password,
-      this.sendForm.value.email
-    );
+    if (this.sendForm.value.name.trim() === '') {
+      this.errorMessage = 'There is no username';
+    } else if (this.sendForm.value.password.trim().length < 8) {
+      this.errorMessage = 'The password must be at least 8 character';
+    } else if (this.sendForm.value.email.trim() === '') {
+      this.errorMessage = 'There is no email';
+    } else {
+      this.tradeApi.register(
+        this.sendForm.value.name.trim(),
+        this.sendForm.value.password.trim(),
+        this.sendForm.value.email.trim()
+      );
+      this.errorMessage = '';
+    }
   }
 
   ngOnInit(): void {}
